4a60be003c540abbdf2194d7691b1b8fb09a03a1
Harshil Darji <darjiharshil2994@gmail.com>
2016-05-10 23:38:06 +0530

    Just added comments

--- src/client/server/Client.java
+++ src/client/server/Client.java
@@ -5,21 +5,36 @@ import java.io.*;
 public class Client {
     public static void main(String[] args) {
         try{
+            //Creating object of Socket class using 127.0.0.1 as IP Address and 7777 as port number.
             Socket s = new Socket("127.0.0.1",7777);
+            
+            //Creating object of PrintStream class.
             PrintStream out = new PrintStream(s.getOutputStream());
+            
+            //Using object of PrintStream to transmit input.
             out.print("1");
-	    out.print("3");
-            out.write(0);
+	        out.print("3");
+            
+            //Appending 0 to notify end of string/input.
+            out.write(0);                   
             out.flush();
+            
+            //Creating object of InputStreamReader to accept data from server.
             InputStreamReader in = new InputStreamReader(s.getInputStream());
             int c;
+            
+            //Reading input from the server untill reached -1 (End of input).
             while((c = in.read()) != -1){
+                //Printing each input character.
                 System.out.print((char)c);
             }
             System.out.println("");
+            
+            //Closing the connection.
             s.close();
         }
         catch(IOException e){
+            //Printing error (if occurred).
             System.err.println(e);
         }
     } 
--- src/client/server/Server.java
+++ src/client/server/Server.java
@@ -5,19 +5,34 @@ import java.io.*;
 public class Server {
     public static void main(String[] args) {
         try{
+            //Creating object of ServerSocket using 7777 as port number(same as provided in client.java).
             ServerSocket ss = new ServerSocket(7777);
+            
+            //Invoking accept() method using object of ServerSocket.
             Socket s  = ss.accept();
+            
+            //Creating object of InputStreamReader to read input from the client.
             InputStreamReader in = new InputStreamReader(s.getInputStream());
             int c,ans=0;
+            
+            //Reading input untill reached 0(End of Input).
             while((c=in.read()) != 0){
+                //Adding numeric value of each input to ans.
                 ans=ans+Character.getNumericValue(c);
             }
+            
+            //Creating object of PrintWriter.
             PrintWriter out = new PrintWriter(s.getOutputStream());
+            
+            //Using object of PrintWriter to transmit output.
             out.print("Answer: "+ans);
             out.flush();
+            
+            //Closing the connection.
             s.close();
         }
         catch(IOException e){
+            //Printing error(if occurred).
             System.err.println(e);
         }
     }
